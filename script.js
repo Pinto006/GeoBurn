bd85847c8631f967135e4b96be9626b89f985739
Wave FC <malloriepinto89@gmail.com>
2023-05-11 20:23:56 -0700

    started local storage function

--- script.js
+++ script.js
@@ -12,16 +12,22 @@
 
 //Ninja API - Dhk9ms+PxG5Q/IrKGhhu1g==FDn9SWMGkkEHOblQ
 //Geoapify API - 7eebaa7dbab54d84b308f239e7d20bbf
-var apiKey = "7eebaa7dbab54d84b308f239e7d20bbf";
-var mapUrl = `https://api.geoapify.com/v1/geocode/search?apiKey=${apiKey}&text=`;
 
+//walk=walk
+//bike = bicycle
+var apiKey = "7eebaa7dbab54d84b308f239e7d20bbf";
+var mapUrl = `https://api.geoapify.com/v1/routing?waypoints=50.679023,4.569876|50.66170,4.578667&mode=${activityEl}&apiKey=7eebaa7dbab54d84b308f239e7d20bbf`;
 var calorieUrl = `https://api.api-ninjas.com/v1/caloriesburned?activity=${activityEl}`;
 var calorieKey = "Dhk9ms+PxG5Q/IrKGhhu1g==FDn9SWMGkkEHOblQ";
 var submitBtn = document.querySelector('#search-btn');
 var activityEl = document.querySelector('#activity');
 var weightEl = document.querySelector('#your-weight');
+var timeEl = document.querySelector('#time-burned');
+var nameEl = document.querySelector('#your-name');
 var startEl = document.querySelector('#starting-point');
 var endEl = document.querySelector('#ending-point');
+
+
 
     
 var getCalories = function (event) {
@@ -30,8 +36,25 @@ var getCalories = function (event) {
     weightEl.value;
     console.log(weightEl.value);
         // fetch(mapUrl)
+    var name = document.querySelector("#your-name").value;
+    var calories = document.querySelector("#calories-burned").value; 
+    var time = document.querySelector("#time-burned").value;
 
+    if (!name || !activityEl || !weightEl || !startEl || !endEl) {
+        return;
+      }
+      fetch (mapUrl + apiKey)
+      .then(function (response) {
+        return response.json();
+      })
+      fetch(calorieUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then 
 };
 
 submitBtn.addEventListener('click', getCalories);
+
+
 
